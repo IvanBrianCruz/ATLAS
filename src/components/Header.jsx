d786e0ba885dd7bed39d13694480c2477b97d9da
IvanBrianCruz <ivan4912134@gmail.com>
2025-03-21 21:50:32 -0300

    banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,21 +11,21 @@ function BasicExample({ setCurrentTab }) {
     filter: 'drop-shadow(0 0 15px red)',
     animation: 'cambioColores 5s infinite',
     cursor: 'pointer',
-
   };
+
   const menu = {
-    padding: '1.5rem 4rem' 
+    padding: '1.5rem 4rem',
   };
+
   const opfinal = {
-
-    color: 'white'
+    color: 'white',
   };
-  const navbarStyle = {
-    backgroundImage: 'url(https://IvanBrianCruz.github.io/ATLAS/img/baner.png)', // URL de la imagen de fondo
-    backgroundSize: 'cover', // Ajusta la imagen para cubrir el área del navbar
-    backgroundPosition: 'center', // Centra la imagen
-    backgroundRepeat: 'no-repeat', // Evita que la imagen se repita
 
+  const navbarStyle = {
+    backgroundImage: 'url(https://IvanBrianCruz.github.io/ATLAS/img/baner.png)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
   };
 
   const keyframes = `
@@ -39,61 +39,79 @@ function BasicExample({ setCurrentTab }) {
   `;
 
   const handleLogoClick = () => {
-    window.location.href = 'https://ivanbriancruz.github.io/ATLAS/', '_blank';
+    window.location.href = 'https://ivanbriancruz.github.io/ATLAS/';
   };
 
   return (
     <>
-      <style>{keyframes}</style>
+      <style>
+        {`
+          @import url('https://fonts.googleapis.com/css2?family=Mokoto:wght@700&display=swap');
+          ${keyframes}
+        `}
+      </style>
+
       <Navbar expand="lg" style={navbarStyle}>
-
-
         <Container>
           <img
-            src='https://ivanbriancruz.github.io/CURSO-ATLAS/img/logo2025.png'
+            src="https://ivanbriancruz.github.io/CURSO-ATLAS/img/logo2025.png"
             alt="Logo Atlas"
             style={logoStyle}
             onClick={handleLogoClick}
           />
           <div style={menu}>
-          <Navbar.Brand 
-  href="https://ivanbriancruz.github.io/ATLAS/" 
-  style={{
-    color: 'white',        // Cambia el color del texto
-    fontSize: '50px',       // Cambia el tamaño de la fuente
-    fontFamily: 'Arial, sans-serif',  // Cambia la fuente
-    fontWeight: 'bold',     // Ajusta el grosor de la fuente
-  }}
->
-  ATLAS
-</Navbar.Brand>
+            <Navbar.Brand
+              href="https://ivanbriancruz.github.io/ATLAS/"
+              style={{
+                color: 'white',
+                fontSize: '70px',
+                fontFamily: "'Mokoto', sans-serif",
+                fontWeight: 'bold',
+              }}
+            >
+              A T L A S
+            </Navbar.Brand>
 
-          <br></br>
-          <br></br>
+
+            <br />
+            <br />
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav >
-                <Nav.Link onClick={() => setCurrentTab('Home')} className="text-white">Home</Nav.Link>
-                <Nav.Link onClick={() => setCurrentTab('introJS')} className="text-white">Introducción a JavaScript</Nav.Link>
-                <Nav.Link onClick={() => setCurrentTab('introHTML')} className="text-white">Introducción a HTML</Nav.Link>
-                <Nav.Link onClick={() => setCurrentTab('introCSS')} className="text-white">Introducción a CSS</Nav.Link>
-                
 
-                <div style={logoStyle}> {/* Envuelve NavDropdown en un div blanco */}
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                <Nav.Link onClick={() => setCurrentTab('Home')} className="text-white">
+                  Home
+                </Nav.Link>
+                <Nav.Link onClick={() => setCurrentTab('introJS')} className="text-white">
+                  Introducción a JavaScript
+                </Nav.Link>
+                <Nav.Link onClick={() => setCurrentTab('introHTML')} className="text-white">
+                  Introducción a HTML
+                </Nav.Link>
+                <Nav.Link onClick={() => setCurrentTab('introCSS')} className="text-white">
+                  Introducción a CSS
+                </Nav.Link>
+
+                <div style={logoStyle}>
                   <NavDropdown title="Complementos" id="basic-nav-dropdown" style={opfinal}>
-                    <NavDropdown.Item onClick={() => setCurrentTab('Evaluacion')}>Evaluación</NavDropdown.Item>
-                    <NavDropdown.Item onClick={() => setCurrentTab('Presentacion')}>presentacion </NavDropdown.Item>
-                    <NavDropdown.Item onClick={() => setCurrentTab('Herramientas')}>Herramientas</NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => setCurrentTab('Evaluacion')}>
+                      Evaluación
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => setCurrentTab('Presentacion')}>
+                      Presentación
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => setCurrentTab('Herramientas')}>
+                      Herramientas
+                    </NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item onClick={() => setCurrentTab('Material')}>material</NavDropdown.Item>
+                    <NavDropdown.Item onClick={() => setCurrentTab('Material')}>
+                      Material
+                    </NavDropdown.Item>
                   </NavDropdown>
                 </div>
-
               </Nav>
             </Navbar.Collapse>
           </div>
-
         </Container>
       </Navbar>
     </>
